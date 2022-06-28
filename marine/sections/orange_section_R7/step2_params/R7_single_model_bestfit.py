98d78aa99ee0e031b21934aee0214574a75a102f
Charles Shobe <charles.shobe@colorado.edu>
2022-06-28 10:10:53 -0400

    add all bestfit scripts; finalize nl ones

--- marine/sections/orange_section_R7/step2_params/R7_single_model_bestfit.py
+++ marine/sections/orange_section_R7/step2_params/R7_single_model_bestfit.py
@@ -1,9 +1,30 @@
+##################################
+#BEST-FIT MODEL RUN FOR ORANGE BASIN SECTION R7
+#RUNS ONE REALIZATION OF THE NONLINEAR, NONLOCAL MODEL 
+#THAT USES BEST-FIT PARAMETER VALUES DETERMINED FROM 
+#PRIOR PARAMETER SPACE SEARCH USING ABC-SMC
+
+#Shobe, C.M., Braun, J., Yuan, X.P., Campforts, B., Gailleton, B., 
+#Baby, G., Guillocheau, F., and Robin, C. (2022) Inverting passive
+#margin stratigraphy for marine sediment transport dynamics over
+#geologic time, Basin Research.
+
+#Please cite the above paper when using this data or code.
+
+#Model and inversion script written by Charlie Shobe (West Virginia University)
+#Archived in 2022 in conjunction with resubmission of Shobe et al. (2022).
+##################################
+
 import numpy as np
-import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
 import xarray as xr
 import xsimlab as xs
+
+#set_run_number
+#used to find the best-fit parameter values from the inversion results
+#as well as to name the output from this best-fit simulation
+run_number = 'ml1'
 
 @xs.process
 class UniformGrid1D:
@@ -25,37 +46,77 @@ class ProfileZ:
     """Compute the evolution of the elevation (z) profile"""
     
     h_vars = xs.group("h_vars") #allows for multiple processes influencing; say diffusion and subsidence
-    #br_vars = xs.group("br_vars") #allows for multiple processes influencing; say diffusion and subsidence
 
     z = xs.variable(
         dims="x", intent="inout", description="surface elevation z", attrs={"units": "m"}
     )
-    #br = xs.variable(
-    #    dims=[(), "x"], intent="in", description="bedrock_elevation", attrs={"units": "m"}
-    #)
+
     br = xs.variable(
         dims=[(), "x"], intent="in", description="bedrock_elevation", attrs={"units": "m"}
     )
     h = xs.variable(
         dims="x", intent="inout", description="sed_thickness", attrs={"units": "m"}
     )
-    
-    dh_compact = xs.variable(
-        dims="x", intent="inout", description="dh_compact", attrs={"units": "m/yr"}
-    )
 
     def run_step(self):
-        #self._delta_br = sum((br for br in self.br_vars))
         self._delta_h = sum((h for h in self.h_vars))
 
     def finalize_step(self):
-        #self.br += self._delta_br #update bedrock surface
         self.h += self._delta_h #update sediment thickness
         self.z = self.br + self.h #add sediment to bedrock to get topo elev.
+        
+import numba as nb
+
+@nb.jit(nopython = True, cache = True)
+def evolve_remaining_nodes(first_marine_node, z, slope, erosion, k_arr, s_crit, travel_dist, sea_level, deposition, qs, spacing, dh_dt, dh ,sed_porosity, dt,h):
+    
+    for i in range(first_marine_node + 1, len(z)): #iterate over each element of x ONLY IN THE MARINE
+        
+        if slope[i] <= 0: #this is the "regular," right-draining case
+            deposition[i] = (qs[i-1] * (1 - np.minimum(1, np.power(slope[i] / s_crit, 2)))) / travel_dist
+            erosion[i] = k_arr[i] * np.abs(slope[i])
+            if z[i] > sea_level:
+                deposition[i] = 0
+        else: #this is the irregular, left-draining case
+            deposition[i] = qs[i-1] / spacing
+            erosion[i] = 0
+            if z[i] > sea_level:
+                deposition[i] = 0
+        dh_dt[i] = (-erosion[i] + deposition[i]) / (1 - sed_porosity)
+
+        dh[i] = dh_dt[i] * dt
+
+        qs[i] = np.maximum(qs[i-1] + (erosion[i] - deposition[i]) * spacing, 0.)
+
+        if -dh[i] > h[i]:
+            dh[i] = -h[i]
+            qs[i] = np.maximum(qs[i-1] + ((-dh[i] / dt) * (1 - sed_porosity)) * spacing, 0.)
+            
+            
+@nb.jit(nopython = True, cache = True)      
+def local_NewtonRaphson_scheme_for_compaction(nn,dh_compact,z0, h, sed_porosity, sed_porosity_depth_scale):
+    #Newton-Raphson iteration  at every node
+        for k in range(nn):
+            fx = 1 #throwaway initial value to trigger "while" loop
+            dfx = 1 #throwaway initial value to trigger "while" loop
+            #check whether we're at the root
+            if dh_compact[k] > 0 : #only apply compaction where deposition is happening
+                while np.abs(fx / dfx) > 1e-6:
+                    #calculate value of function at initial guess
+                    fx = z0[k] - h[k] + sed_porosity * sed_porosity_depth_scale * (np.exp(-z0[k] / sed_porosity_depth_scale) - np.exp(-h[k] / sed_porosity_depth_scale)) - dh_compact[k]
+        
+                    #calculate derivative
+                    dfx = 1 - sed_porosity * np.exp(-z0[k] / sed_porosity_depth_scale)
+                    z0[k] = z0[k] - fx / dfx
+
+            elif dh_compact[k] == 0: #no e or d
+                z0[k] = h[k]
+            else: #in the case where erosion happens, the sediment surface shouldn't rebound. 
+                z0[k] = h[k] + dh_compact[k] / (1 - sed_porosity)
 
 @xs.process
 class ErosionDeposition:
-    """Here's where the actual computation happens: nonlocal diffusion
+    """Here's where the actual computation happenfirst_marine_nodecal diffusion
 
     """
     
@@ -88,15 +149,12 @@ class ErosionDeposition:
     qs = xs.variable(
         dims="x", intent="out", description="qs", attrs={"units": "m2/yr"}
     )
-    #dbr = xs.variable(dims="x", intent="out", groups="br_vars")
     dh = xs.variable(dims="x", intent="out", groups="h_vars")
     
     spacing = xs.foreign(UniformGrid1D, "spacing")
-    #x = xs.foreign(UniformGrid1D, "x")
     z = xs.foreign(ProfileZ, "z")
     br = xs.foreign(ProfileZ, "br")
     h = xs.foreign(ProfileZ, "h")
-    dh_compact = xs.foreign(ProfileZ, "dh_compact")
     
     @xs.runtime(args="step_delta")
     def run_step(self, dt):
@@ -132,7 +190,7 @@ class ErosionDeposition:
 
         else:  #this is the irregular, left-draining case
             self.erosion[first_marine_node] = 0 #because slope = 0
-            self.deposition[first_marine_node] = qs_in / self.spacing #(self.qs_in * (1 + np.minimum(1, np.power(self.slope[first_marine_node] / self.s_crit, 2)))) / self.travel_dist #because slope = 0 #self.qs_in / self.travel_dist
+            self.deposition[first_marine_node] = qs_in / self.spacing
         self.dh_dt[first_marine_node] = (-self.erosion[first_marine_node] + self.deposition[first_marine_node]) / (1 - self.sed_porosity)
         self.dh[first_marine_node] = self.dh_dt[first_marine_node] * dt
         self.qs[first_marine_node] = np.maximum(qs_in + (self.erosion[first_marine_node] - self.deposition[first_marine_node]) * self.spacing, 0.)
@@ -143,94 +201,42 @@ class ErosionDeposition:
         
         
         #evolve remaining marine nodes
-        for i in range(first_marine_node + 1, len(self.z)): #iterate over each element of x ONLY IN THE MARINE
-        
-            if self.slope[i] <= 0: #this is the "regular," right-draining case
-                self.deposition[i] = (self.qs[i-1] * (1 - np.minimum(1, np.power(self.slope[i] / self.s_crit, 2)))) / self.travel_dist
-                self.erosion[i] = k_arr[i] * np.abs(self.slope[i])
-                if self.z[i] > self.sea_level:
-                    self.deposition[i] = 0
-            else: #this is the irregular, left-draining case
-                self.deposition[i] = self.qs[i-1] / self.spacing#(self.qs[i-1] * (1 + np.minimum(1, np.power(self.slope[i] / self.s_crit, 2)))) / self.spacing#self.travel_dist #self.qs[i-1] / self.spacing
-                self.erosion[i] = 0
-                if self.z[i] > self.sea_level:
-                    self.deposition[i] = 0
-            self.dh_dt[i] = (-self.erosion[i] + self.deposition[i]) / (1 - self.sed_porosity)
-            
-            self.dh[i] = self.dh_dt[i] * dt
-            
-            self.qs[i] = np.maximum(self.qs[i-1] + (self.erosion[i] - self.deposition[i]) * self.spacing, 0.)
-            
-            if -self.dh[i] > self.h[i]:
-                self.dh[i] = -self.h[i]
-                self.qs[i] = np.maximum(self.qs[i-1] + ((-self.dh[i] / dt) * (1 - self.sed_porosity)) * self.spacing, 0.)
-            
-        #calculate change in sed thickness
-        #self.dh[:first_marine_node] = 0
-        #self.dh[first_marine_node:] = self.dh_dt[first_marine_node:] * dt
-        #self.dh[self.erosion > self.h] = -self.h[self.erosion > self.h] #if erosion is greater than h, topo only loses h    
-        
+        evolve_remaining_nodes(first_marine_node, self.z, self.slope, self.erosion, k_arr, self.s_crit, 
+                               self.travel_dist, self.sea_level, self.deposition, self.qs, self.spacing,
+                               self.dh_dt, self.dh ,self.sed_porosity, dt, self.h)
+
+
         #compact sediment
         #calculate the thickness after compaction, z0; dh is the thickness of new deposited solid sediment
-        self.dh_compact[:] = self.dh[:] * (1 - self.sed_porosity)
+        dh_compact = self.dh * (1 - self.sed_porosity)
     
         
         #compaction routine
-        #def compaction(porosity, porosity_depth_scale, nn, dh, zi):
         nn = len(self.z)
         z0 = np.zeros(nn)
         #set initial guess for z0:
         z0[:] = self.h[:]
-
-        #Newton-Raphson iteration  at every node
-        for k in range(nn):
-            fx = 1 #throwaway initial value to trigger "while" loop
-            dfx = 1 #throwaway initial value to trigger "while" loop
-            #check whether we're at the root
-            if self.dh_compact[k] > 0 : #only apply compaction where deposition is happening
-                while np.abs(fx / dfx) > 1e-6:
-                    #calculate value of function at initial guess
-                    fx = z0[k] - self.h[k] + self.sed_porosity * self.sed_porosity_depth_scale * (np.exp(-z0[k] / self.sed_porosity_depth_scale) - np.exp(-self.h[k] / self.sed_porosity_depth_scale)) - self.dh_compact[k]
         
-                    #calculate derivative
-                    dfx = 1 - self.sed_porosity * np.exp(-z0[k] / self.sed_porosity_depth_scale)
-                    z0[k] = z0[k] - fx / dfx
+        local_NewtonRaphson_scheme_for_compaction(nn,dh_compact,z0, self.h, self.sed_porosity, self.sed_porosity_depth_scale)
 
-            elif self.dh_compact[k] == 0: #no e or d
-                z0[k] = self.h[k]
-            else: #in the case where erosion happens, the sediment surface shouldn't rebound. 
-                z0[k] = self.h[k] + self.dh_compact[k] / (1 - self.sed_porosity)
-        
         #here, have a chance to set the final dh by differencing new h (z0) and old h (h)
         self.dh[:] = z0[:] - self.h[:]
-        
-        #finalize changes to bedrock (subsidence) and sediment thickness (e/d)
-        #self.dbr = (self.subsidence * dt)
         
 @xs.process
 class InitBasinGeom:
     """
-    Give initial basin elevation field as a function of x:
-    z = exp(- (x - a) / b) + c
+    Set up initial basin geometry
     """
-    
-    #a = xs.variable(description="shift parameter", static=True)
-    #b = xs.variable(description="scale parameter", static=True)
-    #c = xs.variable(description="initial basin floor altitude", static=True)
-    #d = xs.variable(description="x multiplier", static=True)
 
     init_br = xs.variable(dims="x", description="shift parameter", static=True)
     
     x = xs.foreign(UniformGrid1D, "x")
     z = xs.foreign(ProfileZ, "z", intent="out")
-    #br = xs.foreign(ProfileZ, "br", intent="in")
     h = xs.foreign(ProfileZ, "h", intent="out")
-    dh_compact = xs.foreign(ProfileZ, "dh_compact", intent="out")
+
     def initialize(self):
-        #self.br = np.exp(- (self.x * self.d - self.a) / self.b) + self.c #build the initial topography
         self.h = np.zeros(len(self.x)) #initial sediment thickness is 0
-        self.z = np.zeros(len(self.x)) + self.init_br #self.br#(np.exp(- (self.x * self.d - self.a) / self.b) + self.c) + self.h
-        self.dh_compact = np.zeros(len(self.x))
+        self.z = np.zeros(len(self.x)) + self.init_br
         
 marine = xs.Model(
     {
@@ -241,32 +247,41 @@ marine = xs.Model(
     }
 )
 
-#need to import basement elevation and qs time series after they were exported by the prepro notebook
-bedrock_file = '/scratch/cs00048/marine/sections/orange_section_R4/prepro/bedrock_elev_array.npy'
+
+#need to import basement elevation and qs time series after they were exported by the prepro script
+bedrock_file = '../prepro/bedrock_elev_array.npy'
 br = np.load(bedrock_file)
 bedrock_elev_array = xr.DataArray(br, dims=['time', 'x'])
 initial_bedrock = bedrock_elev_array[0, :]
 
-qs_file = '/scratch/cs00048/marine/sections/orange_section_R4/prepro/qs_array.npy'
+qs_file = '../prepro/qs_array.npy'
 qs_array = np.load(qs_file)
 qs_array = xr.DataArray(qs_array, dims=['time'])
-#this is loading in the full m3/yr numbers directly from Baby et al 2019.
 
+#load best fit parameters
+best_fit_params_file = 'best_fit_params_' + str(run_number).zfill(3) + '.npy'
+best_fit_params = np.load(best_fit_params_file)
+best_fit_lambda = best_fit_params[0]
+best_fit_k = best_fit_params[1]
+best_fit_depth_scale = best_fit_params[2]
+best_fit_sc = best_fit_params[3]
+
+#set up model inputs
 in_ds = xs.create_setup(
 	model=marine,
 	clocks={
 		'time': np.arange(0, 130000000, 1000),
-		'otime': np.append(np.arange(0, 130000000, 1000), 129999000)#np.append(np.arange(0, 130000000, 1000000), 129999000) #np.array([19999000])
+		'otime': np.array([0, 17000000, 30000000, 36000000, 49000000, 64000000, 100000000, 119000000, 129999000])
 	},
 	master_clock='time',
 	input_vars={
-		'grid': {'length': 1380000., 'spacing': 10000.},
+			'grid': {'length': 750000., 'spacing': 10000.},
             'init': {'init_br': initial_bedrock},
             'erode': {
-            	'k_factor': 0.1,
-                'k_depth_scale': 100,
-                's_crit': 0.05,
-                'travel_dist': 200000,
+            	'k_factor': best_fit_k,
+                'k_depth_scale': best_fit_depth_scale,
+                's_crit': best_fit_sc,
+                'travel_dist': best_fit_lambda,
                 'sed_porosity': 0.56,
                 'sed_porosity_depth_scale': 2830.,
                 'sea_level': 0.,
@@ -275,10 +290,11 @@ in_ds = xs.create_setup(
             },
 		'profile': {'br': bedrock_elev_array},
    },
-	output_vars={'profile__z': 'otime', 'profile__br': 'otime', 'profile__h': 'otime', 'profile__dh_compact': 'otime'}
+	output_vars={'profile__z': 'otime', 'profile__br': 'otime', 'profile__h': 'otime'}
 )
 
+#run a single instance of the model and store detailed output
 with marine: 
-    out_ds = in_ds.xsimlab.run(store="/scratch/cs00048/marine/sections/orange_section_R4/step2_params/best_fit_XX.zarr")
+    out_ds = in_ds.xsimlab.run(store='best_fit_' + str(run_number).zfill(3) + '.zarr')
     
 print('single model realization finished successfully')
